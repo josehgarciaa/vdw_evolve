d46d4103168d65bf4c7ddba13274c72b38f35104
Jose H. Garcia <josehugo.garcia@protonmail.com>
2022-10-15 15:41:06 +0200

    improve documentation

--- sphinx/source/conf.py
+++ sphinx/source/conf.py
@@ -12,12 +12,12 @@
 # add these directories to sys.path here. If the directory is relative to the
 # documentation root, use os.path.abspath to make it absolute, like shown here.
 #
-# import os
-# import sys
+import os
+import sys
 import kotti_docs_theme
-
-# sys.path.insert(0, os.path.abspath('.'))
-
+sys.path.insert(0, os.path.abspath('../../'))
+cwd = os.getcwd()
+print("current path",cwd)
 
 # -- Project information -----------------------------------------------------
 
@@ -88,7 +88,7 @@ html_theme_path = [kotti_docs_theme.get_theme_dir()]
 # Add any paths that contain custom static files (such as style sheets) here,
 # relative to this directory. They are copied after the builtin static files,
 # so a file named "default.css" will overwrite the builtin "default.css".
-html_static_path = ['_static']
+html_static_path = ['../build/_static']
 
 # Custom sidebar templates, must be a dictionary that maps document names
 # to template names.
