06d9a15c5a808ab9a039b596cacedb64b4903989
Andrei Tomut <tomutvoicuandrei@gmail.com>
2023-02-07 17:08:29 +0100

    dad rectangle shape + +1 fot fit function where a nrbetween (0,1) is squared

---
+++ adaped_solver.py
@@ -0,0 +1,18 @@
+
+import numpy as np
+
+from vdw_evolve import AnnealingSolver
+from vdw_evolve.solvers_utils.fit_function import rectangle_fit_function
+
+from vdw_evolve import allign_along_10
+
+
+# Example
+graphene_cell = np.array([[2.467, 2.467 / 2], [0, 2.467 * np.sqrt(3) / 2]])
+cell = allign_along_10([graphene_cell])[0]
+
+print("\n\nAnnealing_r_Solver:")
+solver1 = AnnealingSolver()
+solver1.fit_f = rectangle_fit_function
+super_cell_annealing = solver1.solve(graphene_cell, np.array([[1,0],[0,1]]))
+print(super_cell_annealing.description_txt())
