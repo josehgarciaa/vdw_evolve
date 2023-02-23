3f0d84cff086f55d050acbf98392b8b9a502c3a3
Jose Hugo Garcia (local) <ljgarcia@1605M010.icn2.net>
2023-02-23 11:53:34 +0100

    started with the testing of the structures

--- examples/0_angle_optimization/optimize.py
+++ examples/0_angle_optimization/optimize.py
@@ -3,11 +3,14 @@ import vdw_evolve as lt
 
 # Read Structure files
 str1 = lt.Structure().read_from("C2_0deg.xyz", format="c2db-xyz")
-str2 = lt.Structure().read_from("C2_45deg.xyz", format="c2db-xyz")
+str2 = lt.Structure().read_from("C2_30deg.xyz", format="c2db-xyz")
 vdws = lt.VdWStructure(str1, str2)
 
+print( vdws.supercell_points(dims=(100,100)) )
+
+#print( vdws.supercell_points(dims=(100,100))  )
 # Optimize using a single core
-vdw_str = lt.VdWStack( vdws, opt_strain=False, opt_angle=True, ncores=1);
+#vdw_str = lt.VdWStack( str1, str2, opt_strain=False, opt_angle=True, ncores=1);
 #vdw_str.write("vdw_optimal.xyz");
 #vdw_str.write("vdw_optimal.json");
 
