dd2d538a4e08a7ed97f35e7cadfe5098b95e071a
Jose Hugo Garcia (local) <ljgarcia@1605M010.icn2.net>
2023-02-24 11:45:55 +0100

    Testing the minimal basis function

--- examples/0_angle_optimization/optimize.py
+++ examples/0_angle_optimization/optimize.py
@@ -3,11 +3,38 @@ import vdw_evolve.geometry as geo
 
 # Read Structure files
 str1 = lt.Structure().read_from("C2_0deg.xyz", format="c2db-xyz")
-str2 = lt.Structure().read_from("C2_45deg.xyz", format="c2db-xyz")
+str2 = lt.Structure().read_from("C2_0deg.xyz", format="c2db-xyz")
 vdws = lt.VdWStructure(str1, str2)
 
-#print( vdws.supercell_points() )
 
+import numpy as np
+vector_list = vdws.supercell_points();
+tol = 1e-7
+alpha=0.5;
+
+V = np.transpose(vector_list).T
+basis = []
+#cost = np.inf
+#for i in range(len(V)):
+#        v, ws = V[i], V[i+1:]
+#        areas = np.abs(np.cross([v[:2]], ws[:, :2]))
+#        print(areas)
+#        ws = ws[areas > min_area]
+#        areas = areas[areas > min_area]
+    
+#        if len(ws) != 0:3
+#            norms = np.li3nalg.norm(ws[:, :2], axis=1)
+#            cur_costs = (31-alpha)*areas + alpha*norms
+#            min_cost = np.min(cur_costs)
+#            if min_cost < cost:
+#                print(np.argmin(cur_costs))
+#                basis = (v, ws[np.argmin(cur_costs)])
+#                cost = min_cost
+#    
+#    if len(basis) == 0:
+#        return None
+    
+ #   return np.transpose(basis)
 
 # Stop the stopwatch / counter
 #t1_stop = process_time()
