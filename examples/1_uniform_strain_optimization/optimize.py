c0750f5d48bc4a7685b738880fc8549f7f50ad28
Jose Hugo Garcia (local) <ljgarcia@1605M010.icn2.net>
2023-03-01 10:32:26 +0100

    Update

--- examples/1_uniform_strain_optimization/optimize.py
+++ examples/1_uniform_strain_optimization/optimize.py
@@ -13,22 +13,22 @@ vdws = lt.VdWStructure(str1, str2)
 
 
 # Define the type of optimization
-#optimizer = lt.LatMatch().OptStrain(False)
+optimizer = lt.LatMatch().UniformStrain(0.1).OptAngle(False)
 
 # Get the minimal cell
-#dims = (10,10)
-#optVdW = vdws.get_minimalcell(dims=dims, optimizer=optimizer)
-#print("Strain: ", optVdW.strain, "angle: ",optVdW.angle)
+dims = (10,10)
+optVdW = vdws.get_minimalcell(dims=dims, optimizer=optimizer)
+print("Strain: ", optVdW.strain, "angle: ", optVdW.angle)
 
 
-#plt.axes().set_aspect('equal')
-#plt.scatter(*(optVdW.host.supercell_points( dims )[:2]), s=16.0)
-#plt.scatter(*(optVdW.complement.supercell_points( dims )[:2]), s=16.0)
+plt.axes().set_aspect('equal')
+plt.scatter(*(optVdW.host.supercell_points(dims)[:2]), s=16.0)
+plt.scatter(*(optVdW.complement.supercell_points(dims)[:2]), s=16.0)
 
 # Parallelogram
-#cell = optVdW.cell[:2,:2]
-#x = [0.0,cell[0][0],cell[0][0]+cell[0][1],cell[0][1],0.0];
-#y = [0.0,cell[1][0],cell[1][0]+cell[1][1],cell[1][1],0.0];
-#plt.gca().add_patch(patches.Polygon(xy=list(zip(x,y)), fill=True, alpha=0.4,color="green"))
-#plt.savefig('matched_lattice.pdf')  
+cell = optVdW.cell[:2,:2]
+x = [0.0,cell[0][0],cell[0][0]+cell[0][1],cell[0][1],0.0];
+y = [0.0,cell[1][0],cell[1][0]+cell[1][1],cell[1][1],0.0];
+plt.gca().add_patch(patches.Polygon(xy=list(zip(x, y)), fill=True, alpha=0.4, color="green"))
+plt.savefig('matched_lattice.pdf')  
 
