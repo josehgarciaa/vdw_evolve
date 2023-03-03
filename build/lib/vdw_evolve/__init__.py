dab8f7edbc9dd615ab0774cad983f6ac02e00ec8
josehgarciaa <josehugo.garcia@protonmail.com>
2023-03-03 15:09:59 +0100

    Clean the version of the code and preserve only latmatch

--- build/lib/vdw_evolve/__init__.py
+++ build/lib/vdw_evolve/__init__.py
@@ -1,11 +1,2 @@
-from .solvers import AnnealingSolver, MechanicSolver, GeneticSolver
-
-from .parser import get_data_as_pd, extract_structure, get_cell_from_structure_file
-
-from .solvers_utils import allign_along_10
-
-from .benchmark import SuperLattice
-
-from .pretty_prints import  plot_unit_cell_vectors
-
-from .structure import SuperCell
+from .structure import Structure, VdWStructure
+from .lat_matcher import LatMatch
