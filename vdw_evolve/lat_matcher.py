fix percentil and degrees as primary units

vdw_evolve/lat_matcher.py
@@ -5,44 +5,56 @@ from .geometry import ChangeBasis, MinimalBasis
 import copy
 
 class LatMatch:
-    opt_angle = True
-    opt_strain = True
-    uni_strain = False
-    angle_range = (-np.pi / 2, np.pi / 2)
-    strain_range = [(-0.05, 0.05), (-0.05, 0.05)]
-    max_dims = (100,100)
+
+    _angle_range = (-np.pi/2, np.pi/2)
+    _strain_range = [(-0.05, 0.05), (-0.05, 0.05)]    
+    _opt_angle = True
+    _opt_strain = True
+    _uni_strain = False
+    max_dims = (100, 100)
     target = None
     reference = None
-    
+    cost = None
+
     def __init__(self, optimize_angle=True, optimize_strain=True):
-        self.opt_angle = optimize_angle
-        self.opt_strain = optimize_strain
+        self._opt_angle = optimize_angle
+        self._opt_strain = optimize_strain
 
-    def OptStrain(self, x=True):
-        self.opt_strain = x
+    def opt_strain(self, x=True):
+        self._opt_strain = x
         return self
 
-    def OptAngle(self, x=True):
-        self.opt_angle = x
+    def opt_angle(self, x=True):
+        self._opt_angle = x
         return self
 
-    def UniformStrain(self, smax, smin=None):
-        if smin is None:
-            smin = -smax
-        self.uni_strain = True
-        self.OptStrain()
-        self.strain_range = [(smin, smax)]
+    def uniform_strain(self, abs_maxstrain, format="perc"):
+        self._uni_strain = True
+        self.opt_strain()
+        smax = abs_maxstrain
+        scal = 1.0
+        if format == "perc":
+            scal = 1/100
+        self._strain_range = [(scal*-smax, scal*smax)]
         return self
    
-    def Strain(self, smax):
-        self.OptStrain()
-        self.max = smax
+    def set_max_strain(self, smax, format="perc"):
+        self.opt_strain()
+        s1, s2 = smax
+        scal = 1.0
+        if format == "perc":
+            scal = 1/100
+        self._strain_range = [(-scal*s1, scal*s1), (-scal*s2, scal*s2)]
         return self
 
-    def AngleRange(self, angle_range):
-        self.OptAngle()
-        self.angle_range = angle_range
-        return None
+    def set_angle_range(self, angle_range, format="deg"):
+        self.opt_angle()
+        amin, amax = angle_range
+        scal = 1.0
+        if format == "deg":
+            scal = np.pi/180.
+        self._angle_range = (scal*amin, scal*amax)
+        return self
 
     def costFuncion(self, r, eta=0.001):
         dx, dy, dz = (r - np.floor(r))
@@ -54,42 +66,67 @@ class LatMatch:
             cost += np.mean(np.exp(- d2 / (2 * etac))) / n ** 2
         return -cost
 
+    def strain_range(self, format="perc"):
+        if format == "perc":
+            return self._strain_range*100
+        return self._strain_range
+
+    def angle_range(self, format="deg"):
+        if format == "deg":
+            return self._angle_range*180/np.pi
+        return self._angle_range
+    
     def unpack_params(self, x):
         s1, s2, angle = 0, 0, 0
 
-        if self.opt_strain:
-            if self.uni_strain:
+        if self._opt_strain:
+            if self._uni_strain:
                 s1 = float(x[0])
                 s2 = s1
             else:
                 s1, s2 = float(x[0]), float(x[1])
-        if self.opt_angle:
+        if self._opt_angle:
             angle = float(x[-1])
         return s1, s2, angle
     
     def fitness(self, x):
         s1, s2, angle = self.unpack_params(x)
-        self.opt = copy.copy(self.target).transform2D((s1, s2), angle, transform_atoms=False)
+        self.opt = copy.copy(self.target).transform2D(strain=(s1, s2),
+                                                      strain_format="abs",
+                                                      angle=angle,
+                                                      angle_format="rad",
+                                                      transform_atoms=False)
         SCpoints = self.opt.supercell_points(self.max_dims)
         rBinA = ChangeBasis(SCpoints, self.reference.cell)
         return self.costFuncion(rBinA)
 
-    def minimalCell(self, target, reference, max_dims=(100, 100),  force=False):
+    def minimalCell(self, target, reference,
+                    max_dims=(100, 100),  force=False):
+        
         self.max_dims = max_dims
         self.target = target
         self.reference = reference
 
         bounds = []
-        if self.opt_strain:
-            bounds += self.strain_range
-        if self.opt_angle:
-            bounds += [self.angle_range]
+        if self._opt_strain:
+            bounds += self._strain_range
+        if self._opt_angle:
+            bounds += [self._angle_range]
 
-        res = optimize.differential_evolution(self.fitness, bounds, maxiter=1000, popsize=1000, polish=True)
+        init_cost = self.fitness((0, 0, 0))
+
+        res = optimize.differential_evolution(self.fitness, bounds,
+                                              maxiter=1000, popsize=1000,
+                                              polish=True)
+        self.cost = res.fun
+        print("Initial cost", init_cost, "Final cost", res.fun, "improvement", res.fun/init_cost )
         s1, s2, angle = self.unpack_params(res.x)
-        self.opt = copy.copy(target).transform2D((s1, s2), angle)
+        self.opt = copy.copy(target).transform2D(strain=(s1, s2),
+                                                 strain_format="abs",
+                                                 angle=angle,
+                                                 angle_format="rad")
+        
         basis = MinimalBasis(self.opt.supercell_points(self.max_dims))
         if basis is None:
             return None
-        
-        return ( ((s1,s2),angle), basis, self.opt)
+        return (((s1, s2), angle), basis, self.opt)

vdw_evolve/structure.py
@@ -39,7 +39,8 @@ class Structure():
         grid = np.mgrid[-nx:nx+1, -ny:ny+1, 0:1].T.reshape((nx*2+1)*(ny*2+1), 3).T
         return (self.cell) @ grid
     
-    def transform2D(self, strain=0, angle=0, transform_atoms=True):
+    def transform2D(self, strain, angle, transform_atoms=True,
+                    angle_format="deg", strain_format="perc"):
         """
             Attributes
             ----------
@@ -53,16 +54,21 @@ class Structure():
                 Apply the transformation to the atoms.
         """
         try:
-            one = np.eye(2, dtype=float)
+            one = np.eye(2).astype(float)
             if isinstance(strain, (int, float)):
                 StrMat = one*strain
             else:
-                StrMat = np.diag(strain)
-            StrMat = one+StrMat/100.0
+                StrMat = np.diag(strain).astype(float)
+                
+            if strain_format == "perc":
+                StrMat = StrMat/100.0
+            StrMat += one
         except ValueError:
             print("Could not properly parse the strain parameters to a  matrix")
             StrMat = None
 
+        if angle_format == "deg":
+            angle = angle*np.pi/180.0
         RotMat = [[np.cos(angle), -np.sin(angle)],
                   [np.sin(angle), np.cos(angle)]]
         try:
@@ -87,11 +93,10 @@ class Structure():
             format: str
                 The angle format, can be either deg (degrees) or rad (radians)
         """
-        if format == "deg":
-            angle = angle*np.pi/180.0
-        return self.transform2D(angle=angle)
+        return self.transform2D(angle=angle, angle_format=format,
+                                strain=0.0)
 
-    def strain2D(self, strain):
+    def strain2D(self, strain, format="perc"):
         """
             Attributes
             ----------
@@ -100,7 +105,8 @@ class Structure():
                 When scalar uniform strain will be applied to all axis
                 When tuple, each value will be apply to an axis
         """
-        return self.transform2D(strain=strain)
+        return self.transform2D(strain=strain, strain_format=format,
+                                angle=0.0)
 
     def ChangeUnitCell(self, new_cell):
         """Change the unit cell and modify the atoms
@@ -121,8 +127,7 @@ class Structure():
                 new_atoms += [(s, p+lat_vec)]
         self.atoms = new_atoms
         self.cell = new_cell
-        
-        return self        
+        return self
 
     def read_from(self, filepath, format):
         """
@@ -179,8 +184,8 @@ class Structure():
 
 class VdWStructure(Structure):
 
-    strain = (0, 0)
-    angle = 0
+    _strain = (0, 0)
+    _angle = 0
 
     def __init__(self, host, complement, supercell=False):
         """
@@ -234,17 +239,42 @@ class VdWStructure(Structure):
 
     def get_minimalcell(self, dims, optimizer):
 
-        (strain, angle), cell, opt_str = optimizer.minimalCell(self.complement, self.host, max_dims=dims)
+        (strain, angle), cell, opt_str = optimizer.minimalCell(self.complement,
+                                                               self.host, max_dims=dims)
         opt_vdw = VdWStructure(self.host, opt_str)
-        opt_vdw.strain = strain
-        opt_vdw.angle = angle
+        opt_vdw._strain = strain
+        opt_vdw._angle = angle
         opt_vdw.host.ChangeUnitCell(cell)
         opt_vdw.complement.ChangeUnitCell(cell)
         opt_vdw.cell = cell
-        opt_vdw.atoms = [ *opt_vdw.host.atoms, *opt_vdw.complement.atoms]
+        opt_vdw.atoms = [*opt_vdw.host.atoms, *opt_vdw.complement.atoms]
         return opt_vdw
 
+    def complement_strain(self, format="perc"):
+        """Return the strain applied to complement to make the vdw cell 
 
+        Attributes
+        ----------
+        format : str, optional
+            The format which can be perc for percentile and abs for absolute, by default "perc"
+        """
+        s1, s2 = self._strain
+        scal = 1.0
+        if format == "perc":
+            scal = 100
+        return (scal*s1, scal*s2)
+    
+    def complement_angle(self, format="deg"):
+        """Return the angle applied to complement to make the vdw cell 
+
+        Attributes
+        ----------
+        format : str, optional
+            The format which can be deg for degrees and rad for radians, by default "deg"
+        """
+        if format == "deg":
+            return self._angle*180/np.pi
+        return self._angle
 
 class SuperCell():
 